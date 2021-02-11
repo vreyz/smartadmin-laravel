c7c9ec417324235b06c0ccc16d6e0675302dd4a7
vreyz <unknown3924@gmail.com>
2021-02-11 10:42:02 +0700

    initial

--- resources/views/SmartAdmin/partials/css.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach($css as $c)
-    <link rel="stylesheet" media="screen, print" href="{{ admin_asset("$c") }}">
-@endforeach
--- resources/views/SmartAdmin/partials/header-logo.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="page-logo">
-    <a href="#" class="page-logo-link press-scale-down d-flex align-items-center position-relative" data-toggle="modal" data-target="#modal-shortcut">
-        <img src="{{ admin_asset("vendor/smartadmin-laravel/smartadmin/img/logo.png")}}" alt="{{config('admin.name')}}" aria-roledescription="logo">
-        <span class="page-logo-text mr-1">{{config('admin.name')}}  {{config('admin.app_version')}}</span>
-        <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2">Laravel {{app()->version()}}</span>
-        <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
-    </a>
-</div>
